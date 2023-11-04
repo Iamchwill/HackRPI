980032b5babdfb5df77ee5ae88aa79143662aafc
Wilde <wildechu23@gmail.com>
2023-11-04 15:22:37 -0400

    more mongo

--- server.py
+++ server.py
@@ -4,9 +4,7 @@ from pymongo.server_api import ServerApi
 
 
 uri = "mongodb+srv://chuw7:psGav2DdUOZXuWYE@toilet.mnkp0e1.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
 
 app = Flask(__name__)
 
