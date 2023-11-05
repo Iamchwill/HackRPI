5fd5bd1ec85bcc7c3ddb4eb4669a6d88650c664e
rspacerr <definitelynotoberon@gmail.com>
2023-11-04 22:55:46 -0400

    location to review form

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+// Function to update the location input with the current location
+function updateLocationWithCurrentLocation() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const locationInput = document.getElementById("locat");
+            locationInput.value = `${latitude}, ${longitude}`;
+        }, function (error) {
+            console.error("Error getting current location:", error);
+        });
+    } else {
+        console.error("Geolocation not supported by the browser.");
+    }
+}
+
+// Add a click event listener to the "Use Current Location" button
+const useCurrentLocationButton = document.getElementById("useCurrentLocation");
+useCurrentLocationButton.addEventListener("click", updateLocationWithCurrentLocation);
