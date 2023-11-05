e61be28e1faf28b135d63be95fb2f51f6c65e7fd
Wilde <wildechu23@gmail.com>
2023-11-04 22:51:58 -0400

    some routing stuff

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+import route
+import math
+
 def insert_toilet(collection, location, genders, facilities):
     new_toilet = {
         'location': location,
@@ -7,7 +10,6 @@ def insert_toilet(collection, location, genders, facilities):
         'toilet': facilities[0],
         'urinal': facilities[1],
         'sink': facilities[2],
-        'soap': facilities[3],
     }
     collection.insert_one(new_toilet)
 
@@ -19,7 +21,6 @@ def edit_location(collection, tid, new_location, genders, facilities):
             'toilet': facilities[0],
             'urinal': facilities[1],
             'sink': facilities[2],
-            'soap': facilities[3],
         } 
     })
 
@@ -31,4 +32,16 @@ def add_review(collection, tid, quality):
     toilet['quality'] = (q * r + quality) / (r + 1)
     toilet['reviews'] += 1
     collection.replace_one({'_id': tid}, toilet)
+
+
+def find_closest_toilets(collection, location):
+    for document in collection:
+        coords = {document['location'][0], document['location'][1]}
+        r = route.fastest_route({location[0],location[1]}, coords)
+        edge_lengths = osmnx.utils_graph.get_route_edge_attributes(G, r, 'length')
+        route_length = sum(edge_lengths)
+        print(route_length)
+
+def dist(pt1, pt2):
+    return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1]-pt2[1])**2)
 
